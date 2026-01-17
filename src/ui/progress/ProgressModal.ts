7eb918fb8ab10dee13185f6a2c2f94fb00b25227
Richard Parks <richardparks@Richards-MacBook-Air.local>
2026-01-17 14:09:57 -0800

    Adding in AGPL 3 licensing.

--- src/ui/progress/ProgressModal.ts
+++ src/ui/progress/ProgressModal.ts
@@ -1,3 +1,32 @@
+/*
+ * Copyright (C) 2026 The Application Foundry, LLC 
+ *
+ * This file is part of NoteMakerAI.
+ *
+ * NoteMakerAI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NoteMakerAI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * =========================================================================
+ *
+ * COMMERCIAL LICENSE OPTION
+ *
+ * If you wish to use this software in a proprietary product or are unable
+ * to comply with the terms of the AGPLv3, a commercial license is available.
+ *
+ * For commercial licensing inquiries, please contact: license@theapplicationfoundry.com 
+ *
+ * =========================================================================
+ */
 import { App, Modal } from 'obsidian';
 
 export class ProgressModal extends Modal {
